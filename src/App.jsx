a46fcc56d5ab982fa8b6f40b614ebb366912ea5a
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-01 18:56:32 +1100

    Use `<MerchantContext>`

--- src/App.jsx
+++ src/App.jsx
@@ -12,50 +12,59 @@ import CustomerProductsPage from "./components/CustomerPages/CustomerProductsPag
 import CustomerCartPage from "./components/CustomerPages/CustomerCartPage";
 import CustomerOrderConfirmation from "./components/CustomerPages/CustomerOrderConfirmation";
 import { useState } from "react";
-import { LoggedInUserContext } from "./utils/LoggedInUserContext";
+import { AuthContext } from "./utils/AuthContext";
+import { MerchantContext } from "./utils/MerchantContext";
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState("");
+  const [merchant, setMerchant] = useState("");
   const [token, setToken] = useState("");
   return (
     <div className="App">
-      <LoggedInUserContext.Provider
-        value={{ loggedInUser, setLoggedInUser, token, setToken }}
+      <AuthContext.Provider
+        value={{
+          loggedInUser,
+          setLoggedInUser,
+          token,
+          setToken,
+        }}
       >
-        <NavLink
-          key="fruGo-title"
-          style={{
-            textDecoration: "none",
-          }}
-          to="/"
-        >
-          <h1 className="fruGo-title">fruGo</h1>
-        </NavLink>
-        <Routes>
-          <Route exact path="/" element={<NotLoggedInHomePage />} />
+        <MerchantContext.Provider value={{ merchant, setMerchant }}>
+          <NavLink
+            key="fruGo-title"
+            style={{
+              textDecoration: "none",
+            }}
+            to="/"
+          >
+            <h1 className="fruGo-title">fruGo</h1>
+          </NavLink>
+          <Routes>
+            <Route exact path="/" element={<NotLoggedInHomePage />} />
 
-          <Route exact path="products" element={<ProductsPage />} />
+            <Route exact path="products" element={<ProductsPage />} />
 
-          <Route exact path="login" element={<Login />} />
+            <Route exact path="login" element={<Login />} />
 
-          <Route exact path="register" element={<Register />} />
+            <Route exact path="register" element={<Register />} />
 
-          <Route exact path="customer" element={<CustomerHomePage />} />
-          <Route
-            exact
-            path="customer/products"
-            element={<CustomerProductsPage />}
-          />
-          <Route exact path="customer/cart" element={<CustomerCartPage />} />
-          <Route
-            exact
-            path="customer/order-confirmation"
-            element={<CustomerOrderConfirmation />}
-          />
+            <Route exact path="customer" element={<CustomerHomePage />} />
+            <Route
+              exact
+              path="customer/products"
+              element={<CustomerProductsPage />}
+            />
+            <Route exact path="customer/cart" element={<CustomerCartPage />} />
+            <Route
+              exact
+              path="customer/order-confirmation"
+              element={<CustomerOrderConfirmation />}
+            />
 
-          <Route exact path="*" element={<NotFound />} />
-        </Routes>
-      </LoggedInUserContext.Provider>
+            <Route exact path="*" element={<NotFound />} />
+          </Routes>
+        </MerchantContext.Provider>
+      </AuthContext.Provider>
     </div>
   );
 }

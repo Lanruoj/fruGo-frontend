8143cc8af4ad4f64ec447b9f6850ab031bafe7d4
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-01 18:56:55 +1100

    Set `merchant` & `localStorage.token`

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 import NotLoggedInNavBar from "./NavBar/NotLoggedInNavBar";
 import Title from "./styled/Title";
 import InputWrapper from "./styled/InputWrapper";
-import { useLoggedInUserContext } from "../utils/LoggedInUserContext";
+import { useAuthContext } from "../utils/AuthContext";
+import { useMerchantContext } from "../utils/MerchantContext";
 
 export default function Login(props) {
-  const { setLoggedInUser, setToken } = useLoggedInUserContext();
+  const { setLoggedInUser, setToken } = useAuthContext();
+  const { setMerchant } = useMerchantContext();
   const [userFormDetails, setUserFormDetails] = useState({
     username: "",
     password: "",
@@ -23,12 +25,20 @@ export default function Login(props) {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const response = await axios.post("/auth/login", userFormDetails);
+
     if (response.status == 200) {
       setLoggedInUser(() => {
         return response.data.user;
       });
       setToken(() => {
+        localStorage.setItem("token", response.data.accessToken);
         return response.data.accessToken;
+      });
+      const merchantResponse = await axios.get(
+        `/merchants/${response.data.cart._merchant}`
+      );
+      setMerchant(() => {
+        return merchantResponse.data.data;
       });
     }
   };

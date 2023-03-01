da19b0b4b2e5cabc9f8fe0c72f0a2f30fb17adef
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-01 18:57:49 +1100

    Fetch `/stock/products`

--- src/components/ProductsPage/ProductsPage.jsx
+++ src/components/ProductsPage/ProductsPage.jsx
@@ -3,16 +3,30 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import NotLoggedInNavBar from "../NavBar/NotLoggedInNavBar";
 import { ProductList } from "./ProductList";
+import { useAuthContext } from "../../utils/AuthContext";
+import { useMerchantContext } from "../../utils/MerchantContext";
 
 export function ProductsPage() {
+  const { loggedInUser } = useAuthContext();
+  const { merchant } = useMerchantContext();
   const [products, setProducts] = useState("");
   useEffect(() => {
-    axios
-      .get("/products")
-      .then((res) => res.data.data)
-      .then((data) => {
-        setProducts(data);
-      });
+    if (!loggedInUser) {
+      axios
+        .get("/products")
+        .then((res) => res.data.data)
+        .then((data) => {
+          setProducts(data);
+        });
+    } else {
+      axios
+        .get(`/merchants/${merchant._id}/stock/products`)
+        .then((res) => res.data.data)
+        .then((data) => {
+          console.log(data);
+          setProducts(data);
+        });
+    }
   }, []);
   return (
     <>

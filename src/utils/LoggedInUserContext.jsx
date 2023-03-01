ea85643233d25b533c1519d222a355ed533ed947
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-01 18:58:18 +1100

    Delete `LoggedInUserContext`

--- src/utils/LoggedInUserContext.jsx
+++
@@ -1,4 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const LoggedInUserContext = createContext("");
-export const useLoggedInUserContext = () => useContext(LoggedInUserContext);

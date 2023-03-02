8c170e207f527160491dcfa335a7df4c0f96f632
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-02 12:32:27 +1100

    Create `handleAddToCart()`

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,40 @@
 import { Button } from "@mui/material";
+import axios from "axios";
+import { useCartContext } from "../utils/CartContext";
 import { Wrapper } from "./styled/Wrapper";
 
 export const Product = (props) => {
   const { product } = props;
+  const { cart, setCart } = useCartContext();
+  const handleAddToCart = (event) => {
+    event.preventDefault();
+    if (
+      cart._cartProducts.some(
+        (cartProduct) => cartProduct._stockProduct._id == event.target.value
+      )
+    ) {
+      console.log("Product already in cart");
+    } else {
+      axios
+        .post(
+          `/customers/${
+            JSON.parse(localStorage.getItem("user"))._id
+          }/cart/products`,
+          {
+            product: event.target.value,
+          }
+        )
+        .then((response) => {
+          setCart(() => {
+            if (cart) {
+              console.log("Product added to cart");
+              localStorage.setItem("cart", JSON.stringify(response.data.cart));
+              return response.data.cart;
+            }
+          });
+        });
+    }
+  };
   return (
     <Wrapper>
       <img
@@ -29,7 +61,23 @@ export const Product = (props) => {
       >
         ${product.price || product.product.price}
       </div>
-      <Button>Add to cart</Button>
+      {!cart && (
+        <Button value={product._id} onClick={handleAddToCart} disabled={true}>
+          Add to cart
+        </Button>
+      )}
+      {cart && (
+        <Button
+          value={product._id}
+          onClick={handleAddToCart}
+          disabled={cart._cartProducts.some(
+            (cartProduct) =>
+              cartProduct._stockProduct._id == product._id && true
+          )}
+        >
+          Add to cart
+        </Button>
+      )}
     </Wrapper>
   );
 };

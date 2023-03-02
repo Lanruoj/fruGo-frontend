dfe49f7a4041d56aef9cce911cef09bb49852a92
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-02 17:51:25 +1100

    Create `handleSubmitOrder()`

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,17 @@
+import { Button } from "@mui/material";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { useCartContext } from "../utils/CartContext";
 
 const CartContainer = styled.div`
   background-color: red;
-  height: 100vh;
+  height: 80vh;
   width: 100vw;
 `;
 
 const CartProductList = styled.ul`
   background-color: blue;
-  height: 1000px;
-  width: 1000px;
   flex-direction: column;
   align-items: center;
 `;
@@ -38,34 +39,44 @@ const CartProductImg = styled.img`
 `;
 
 export const Cart = () => {
-  const { cartProducts } = useCartContext();
+  const { cartProducts, setCartProducts } = useCartContext();
+  const navigate = useNavigate();
+  const handleSubmitOrder = (event) => {
+    axios.post("/orders").then((response) => {
+      console.log(response);
+      setCartProducts([]);
+      navigate("/orders");
+    });
+  };
   return (
     <CartContainer>
       <h1>Cart</h1>
       <CartProductList>
-        {cartProducts.length &&
-          cartProducts.map((cartProduct) => {
-            return (
-              <CartProduct key={cartProduct._id}>
-                <CartProductName>
-                  {cartProduct._stockProduct._product.name}
-                </CartProductName>
-                <CartProductImg
-                  src={cartProduct._stockProduct._product.img}
-                ></CartProductImg>
-                <CartProductQuantity>
-                  Quantity:
-                  {cartProduct.subQuantity}
-                </CartProductQuantity>
-                <CartProductPrice>
-                  Subtotal:
-                  {cartProduct._stockProduct._product.price *
-                    cartProduct.subQuantity}
-                </CartProductPrice>
-              </CartProduct>
-            );
-          })}
+        {cartProducts.length
+          ? cartProducts.map((cartProduct) => {
+              return (
+                <CartProduct key={cartProduct._id}>
+                  <CartProductName>
+                    {cartProduct._stockProduct._product.name}
+                  </CartProductName>
+                  <CartProductImg
+                    src={cartProduct._stockProduct._product.img}
+                  ></CartProductImg>
+                  <CartProductQuantity>
+                    Quantity:
+                    {cartProduct.subQuantity}
+                  </CartProductQuantity>
+                  <CartProductPrice>
+                    Subtotal:
+                    {cartProduct._stockProduct._product.price *
+                      cartProduct.subQuantity}
+                  </CartProductPrice>
+                </CartProduct>
+              );
+            })
+          : "No products in cart"}
       </CartProductList>
+      <Button onClick={handleSubmitOrder}>Submit order</Button>
     </CartContainer>
   );
 };

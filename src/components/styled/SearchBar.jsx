ea372463b2902b3eee8bdc4cc320b1931fe75a6c
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-03 21:05:48 +1100

    Style `<SearchBar>`

--- src/components/styled/SearchBar.jsx
+++ src/components/styled/SearchBar.jsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const SearchBar = styled.input.attrs({ type: "search" })`
   margin: 1.5rem;
-  width: 15rem;
-  height: 3rem;
+  width: 10rem;
+  height: 2rem;
 `;

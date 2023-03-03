a4a84b653ce3317f7916b9e809ecab13907194f5
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-03 21:05:39 +1100

    Style `<Main>`

--- src/components/styled/Main.jsx
+++ src/components/styled/Main.jsx
@@ -2,4 +2,9 @@ import styled from "styled-components";
 
 export const Main = styled.main`
   padding-bottom: 5rem;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;

0e04137205c013b9bd7691b0ac3a4434126da12c
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-03 16:52:36 +1100

    Remove redundant imports

--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -2,21 +2,11 @@ import axios from "axios";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { useAuthContext } from "../utils/AuthContext";
-import { useMerchantContext } from "../utils/MerchantContext";
-import { GridBox } from "./styled/GridBox";
-import { Product } from "./Product";
-import { useCartContext } from "../utils/CartContext";
 import { SearchBar } from "./styled/SearchBar";
 import { StockProduct } from "./StockProduct";
 
-export const CustomGrid = styled(GridBox)`
-  padding: 30px;
-`;
-
 export function Stock() {
   const { loggedInUser } = useAuthContext();
-  const { cartProducts } = useCartContext();
-  const { merchant } = useMerchantContext();
   const [products, setProducts] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
   useEffect(() => {
@@ -48,14 +38,12 @@ export function Stock() {
           placeholder="Search for products"
         />
       </form>
-      <div id="products">
-        <GridBox>
-          {products &&
-            products.map((product) => {
-              return <StockProduct key={product._id} product={product} />;
-            })}
-          {!products.length && <h2>No products matching that criteria</h2>}
-        </GridBox>
+      <div>
+        {products &&
+          products.map((product) => {
+            return <StockProduct key={product._id} product={product} />;
+          })}
+        {!products.length && <h2>No products matching that criteria</h2>}
       </div>
     </>
   );

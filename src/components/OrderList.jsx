6de20f440bff1d3cce90f664f9646f0d3b800d66
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-04 13:31:29 +1100

    Refactor `<OrderList>` to be universal

--- src/components/OrderList.jsx
+++ src/components/OrderList.jsx
@@ -4,22 +4,51 @@ import { useAuthContext } from "../utils/AuthContext";
 import { Order } from "./Order";
 
 export const OrderList = (props) => {
-  const { loggedInUser } = useAuthContext();
+  const { loggedInUser, role } = useAuthContext();
   const [orders, setOrders] = useState([]);
+  const [statusFilter, setStatusFilter] = useState("");
   useEffect(() => {
-    axios.get(`/customers/${loggedInUser._id}/orders`).then((response) => {
-      setOrders(() => {
-        return response.data.data;
-      });
+    if (role == "Customer") {
+      axios
+        .get(`/customers/${loggedInUser._id}/orders${statusFilter}`)
+        .then((response) => {
+          setOrders(() => {
+            return response.data.data;
+          });
+        });
+    } else if (role == "Merchant") {
+      axios
+        .get(`/customers/${loggedInUser._id}/orders${statusFilter}`)
+        .then((response) => {
+          console.log(response.data.data);
+          setOrders(() => {
+            return response.data.data;
+          });
+        });
+    }
+  }, [statusFilter]);
+  const handleStatusFilter = (event) => {
+    event.preventDefault();
+    setStatusFilter(() => {
+      return event.target.value;
     });
-  }, []);
+  };
   return (
     <>
       <h1>Orders</h1>
+      <select onChange={handleStatusFilter}>
+        <option value="?status=pending">Pending</option>
+        <option value="?status=complete">Complete</option>
+        <option value="?status=cancelled">Cancelled</option>
+      </select>
       <div>
-        {orders.map((order) => {
-          return <Order key={order._id} order={order} />;
-        })}
+        {orders.length ? (
+          orders.map((order) => {
+            return <Order key={order._id} order={order} />;
+          })
+        ) : (
+          <p>You currently have no {statusFilter.split("=")[1]} orders</p>
+        )}
       </div>
     </>
   );

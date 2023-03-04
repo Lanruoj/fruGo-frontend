8b155e4bf0e34bee3eb6c2f2d1781e7fab40402c
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-04 12:52:08 +1100

    Style `<Order>` elements

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,43 +1,73 @@
+import styled from "styled-components";
+
+const OrderContainer = styled.div`
+  border: solid red;
+`;
+
+const OrderDetails = styled.ul`
+  list-style: none;
+`;
+const OrderProducts = styled.ul`
+  list-style: none;
+`;
+const CustomerDetails = styled.ul`
+  list-style: none;
+  text-align: left;
+`;
+
+const OrderProduct = styled.li`
+  text-align: left;
+  margin: 1rem 0 1rem 0;
+`;
+
 export const Order = (props) => {
   const { order } = props;
   return (
-    <div>
+    <OrderContainer>
       <div>
         <b>Order no:</b> {order._id}
       </div>
-      <h2>Products</h2>
-      <ul>
-        {order &&
-          order._orderProducts.map((orderProduct) => {
-            return (
-              <li key={orderProduct._id}>
-                <div>
-                  <h3>
-                    <b>{orderProduct.stockProduct._product.name}</b>
-                  </h3>
-                </div>
-                <img
-                  width="100px"
-                  src={orderProduct.stockProduct._product.img}
-                />
-                <div>
-                  <b>Quantity:</b> {orderProduct.quantity}
-                </div>
-                <div>
-                  $
-                  {Number.parseFloat(
-                    orderProduct.stockProduct._product.price *
-                      orderProduct.quantity
-                  ).toFixed(2)}
-                </div>
-                <div>
-                  <b>Total price: </b>$
-                  {Number.parseFloat(order.totalPrice).toFixed(2)}
-                </div>
-              </li>
-            );
-          })}
-      </ul>
-    </div>
+      <OrderDetails>
+        <CustomerDetails>
+          <div>
+            <b>Customer:</b>{" "}
+            {`${order._customer.firstName} ${order._customer.lastName}`}
+          </div>
+          <div>
+            <b>Address:</b>{" "}
+            {`${order._customer.streetAddress}, ${order._customer._city.name}`}
+          </div>
+        </CustomerDetails>
+        <OrderProducts>
+          {order &&
+            order._orderProducts.map((orderProduct) => {
+              return (
+                <OrderProduct key={orderProduct.stockProduct._id}>
+                  <div>
+                    <b>Name:</b> {orderProduct.stockProduct._product.name}
+                  </div>
+                  <div>
+                    <b>Serial no:</b> {orderProduct.stockProduct._id}
+                  </div>
+
+                  <div>
+                    <b>Quantity:</b> {orderProduct.quantity}
+                  </div>
+                  <div>
+                    <b>Subtotal:</b> $
+                    {Number.parseFloat(
+                      orderProduct.stockProduct._product.price *
+                        orderProduct.quantity
+                    ).toFixed(2)}
+                  </div>
+                </OrderProduct>
+              );
+            })}
+        </OrderProducts>
+        <div>
+          <b>Total price: </b>${Number.parseFloat(order.totalPrice).toFixed(2)}
+        </div>
+      </OrderDetails>
+    </OrderContainer>
   );
 };

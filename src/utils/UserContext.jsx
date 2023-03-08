37fce795957ef4a0982c6542ae5c37a51f16507b
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-08 11:18:57 +1100

    Create `UserContextProvider`

--- src/utils/UserContext.jsx
+++ src/utils/UserContext.jsx
@@ -1,4 +1,26 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { authenticateUser } from "./auth";
+import { Login } from "../components/Login";
 
 export const UserContext = createContext("");
 export const useUserContext = () => useContext(UserContext);
+
+export const UserContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState("");
+  useEffect(() => {
+    const isLoggedIn = async () => {
+      let user = authenticateUser();
+      if (!user) {
+        localStorage.setItem("user", "");
+        user = "";
+      }
+      setCurrentUser(user);
+    };
+    isLoggedIn();
+  }, []);
+  return (
+    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+      {currentUser?.token ? children : <Login />}
+    </UserContext.Provider>
+  );
+};

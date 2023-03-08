d39b34c31abb2efa4731e2387d9bad0d641b3cd7
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-08 15:45:15 +1100

    Create `currentToken` state

--- src/utils/UserContext.jsx
+++ src/utils/UserContext.jsx
@@ -6,21 +6,25 @@ import { useLocation } from "react-router-dom";
 export const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
+  const [currentToken, setCurrentToken] = useState("");
   const [currentUser, setCurrentUser] = useState("");
   const [currentRole, setCurrentRole] = useState("");
   const [cart, setCart] = useState("");
-  const [session, setSession] = useState("");
+  const [session, setSession] = useState(1);
   useEffect(() => {
     const isLoggedIn = async () => {
-      let { user, role, cart } = authenticateUser();
+      let { token, user, role, cart } = authenticateUser();
       if (!user) {
+        localStorage.setItem("token", "");
         localStorage.setItem("user", "");
         localStorage.setItem("role", "");
         localStorage.setItem("cart", "");
+        token = "";
         user = "";
         role = "";
         cart = "";
       }
+      setCurrentToken(token);
       setCurrentUser(user);
       setCurrentRole(role);
       setCart(cart);
@@ -32,6 +36,8 @@ export const UserContextProvider = ({ children }) => {
       value={{
         currentUser,
         setCurrentUser,
+        currentToken,
+        setCurrentToken,
         currentRole,
         setCurrentRole,
         cart,

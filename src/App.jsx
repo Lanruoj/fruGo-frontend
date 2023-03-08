b246af4a83f473dc2d39ec3e4e7b2ed2eb8010fd
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-08 22:37:34 +1100

    Extract `<Routes>` into separate `<Router>` component

--- src/App.jsx
+++ src/App.jsx
@@ -1,129 +1,16 @@
-import axios from "axios";
-import {
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
-import { useState, useEffect } from "react";
-import { Home } from "./components/Home";
-import { CustomerProducts } from "./components/CustomerProducts";
-import { Login } from "./components/Login";
+import { Router } from "./components/Router";
 import { NavBar } from "./components/NavBar";
-import { Register } from "./components/Register";
-import "./App.css";
-import { Cart } from "./components/Cart";
-import { OrderList } from "./components/OrderList";
-import { OrderConfirmation } from "./components/OrderConfirmation";
-import { Stock } from "./components/Stock";
-import { CustomerRoute } from "./utils/CustomerRoute";
 import { Main } from "./components/styled/Main";
-import { MerchantRoute } from "./utils/MerchantRoute";
-import { UserContext, UserContextProvider } from "./utils/UserContext";
-import { AddNewStockProduct } from "./components/AddNewStockProduct";
-import { UserProfile } from "./components/UserProfile";
+import { UserContextProvider } from "./utils/UserContext";
+import "./App.css";
 
 function App() {
-  const [userLoggedIn, setUserLoggedIn] = useState("");
-  useEffect(() => {
-    if (localStorage.getItem("user")) {
-      setUserLoggedIn(true);
-    } else {
-      setUserLoggedIn(false);
-    }
-  });
   return (
     <div className="App">
       <UserContextProvider>
         <NavBar />
         <Main>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route
-              exact
-              path="/login"
-              element={userLoggedIn ? <Navigate to="/" /> : <Login />}
-            />
-            <Route
-              exact
-              path="/customer/register"
-              element={
-                userLoggedIn == "Customer" ? <Navigate to="/" /> : <Register />
-              }
-            />
-            <Route path="customer">
-              <Route
-                path="profile"
-                element={
-                  <CustomerRoute>
-                    <UserProfile />
-                  </CustomerRoute>
-                }
-              />
-              <Route index path="products" element={<CustomerProducts />} />
-              <Route
-                path="cart"
-                element={
-                  <CustomerRoute>
-                    <Cart />
-                  </CustomerRoute>
-                }
-              />
-              <Route
-                path="orders"
-                element={
-                  <CustomerRoute>
-                    <OrderList />
-                  </CustomerRoute>
-                }
-              />
-              <Route
-                path={`orderConfirmation/:orderID`}
-                element={
-                  <CustomerRoute>
-                    <OrderConfirmation />
-                  </CustomerRoute>
-                }
-              />
-            </Route>
-            <Route path="merchant">
-              <Route path="stock">
-                <Route
-                  index
-                  element={
-                    <MerchantRoute>
-                      <Stock />
-                    </MerchantRoute>
-                  }
-                />
-                <Route
-                  path="add"
-                  element={
-                    <MerchantRoute>
-                      <AddNewStockProduct />
-                    </MerchantRoute>
-                  }
-                />
-              </Route>
-              <Route
-                path="profile"
-                element={
-                  <MerchantRoute>
-                    <UserProfile />
-                  </MerchantRoute>
-                }
-              />
-              <Route
-                path="orders"
-                element={
-                  <MerchantRoute>
-                    <OrderList />
-                  </MerchantRoute>
-                }
-              />
-            </Route>
-          </Routes>
+          <Router />
         </Main>
       </UserContextProvider>
     </div>

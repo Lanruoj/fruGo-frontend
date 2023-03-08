0e25131edce28fbe62a41e0b220fba7ed37d006c
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-08 13:21:29 +1100

    Set `cart`

--- src/utils/UserContext.jsx
+++ src/utils/UserContext.jsx
@@ -7,20 +7,40 @@ export const useUserContext = () => useContext(UserContext);
 
 export const UserContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState("");
+  const [currentRole, setCurrentRole] = useState("");
+  const [cart, setCart] = useState("");
   useEffect(() => {
     const isLoggedIn = async () => {
-      let user = authenticateUser();
+      let { user, role, cart } = authenticateUser();
       if (!user) {
         localStorage.setItem("user", "");
+        localStorage.setItem("role", "");
+        localStorage.setItem("cart", "");
         user = "";
+        role = "";
+        cart = "";
       }
       setCurrentUser(user);
+      setCurrentRole(role);
+      setCart(cart);
     };
     isLoggedIn();
   }, []);
+  useEffect(() => {
+    console.log(currentUser);
+  }, [currentUser]);
   return (
-    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
-      {currentUser?.token ? children : <Login />}
+    <UserContext.Provider
+      value={{
+        currentUser,
+        setCurrentUser,
+        currentRole,
+        setCurrentRole,
+        cart,
+        setCart,
+      }}
+    >
+      {children}
     </UserContext.Provider>
   );
 };

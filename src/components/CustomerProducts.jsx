f10d99b64040afa8dbd058779ae5879c805a5f16
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-08 14:56:54 +1100

    Update `CustomerProducts` with new states

--- src/components/CustomerProducts.jsx
+++ src/components/CustomerProducts.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
-import { GridBox } from "./styled/GridBox";
 import { Product } from "./Product";
 import { SearchBar } from "./styled/SearchBar";
 import { useUserContext } from "../utils/UserContext";
@@ -15,8 +14,7 @@ export const RowWrapper = styled.div`
 `;
 
 export const CustomerProducts = () => {
-  const { currentUser, currentRole, setCurrentUser, setCurrentRole, cart } =
-    useUserContext();
+  const { currentUser, cart } = useUserContext();
   const [products, setProducts] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   useEffect(() => {
@@ -26,11 +24,10 @@ export const CustomerProducts = () => {
           `/merchants/${currentUser._merchant._id}/stock/products?name=${searchQuery}`
         )
         .then((response) => {
-          console.log(response.data.data);
           setProducts(response.data.data);
         });
     }
-  }, [searchQuery]);
+  }, [currentUser]);
   const handleSearchQueryChange = (event) => {
     event.preventDefault();
     setSearchQuery(event.target.value);
@@ -48,9 +45,11 @@ export const CustomerProducts = () => {
       <RowWrapper>
         {products &&
           products.map((product) => {
-            const existingProduct = cart.find(
-              (cartProduct) => cartProduct.stockProduct._id == product._id
-            );
+            const existingProduct = cart.length
+              ? cart.find(
+                  (cartProduct) => cartProduct.stockProduct._id == product._id
+                )
+              : null;
             return (
               <Product
                 key={product._id}

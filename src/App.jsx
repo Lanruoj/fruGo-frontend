ff3fbacd0838ac0f3ecb177b1effa888f07355a3
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-08 13:18:28 +1100

    Disable all state hooks

--- src/App.jsx
+++ src/App.jsx
@@ -14,86 +14,67 @@ import { Stock } from "./components/Stock";
 import { CustomerRoute } from "./utils/CustomerRoute";
 import { Main } from "./components/styled/Main";
 import { MerchantRoute } from "./utils/MerchantRoute";
-import { UserContext } from "./utils/UserContext";
+import { UserContext, UserContextProvider } from "./utils/UserContext";
 import { AddNewStockProduct } from "./components/AddNewStockProduct";
 import { UserProfile } from "./components/UserProfile";
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem("token") || "");
-  const [loggedInUser, setLoggedInUser] = useState(
-    localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : ""
-  );
-  const [role, setRole] = useState(localStorage.getItem("role") || "");
-  const [merchant, setMerchant] = useState(
-    localStorage.getItem("merchant")
-      ? JSON.parse(localStorage.getItem("merchant"))
-      : ""
-  );
-  const [cartProducts, setCartProducts] = useState([]);
-  const [newOrder, setNewOrder] = useState("");
-  const [customerProducts, setCustomerProducts] = useState([]);
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (loggedInUser) {
-      setToken(() => {
-        return localStorage.getItem("token");
-      });
-      setRole(() => {
-        return localStorage.getItem("role");
-      });
-      if (localStorage.getItem("role") == "Customer") {
-        setMerchant(() => {
-          return JSON.parse(localStorage.getItem("merchant"));
-        });
-        axios
-          .get(
-            `/merchants/${
-              JSON.parse(localStorage.getItem("merchant"))._id
-            }/stock/products`
-          )
-          .then((response) => {
-            setCustomerProducts(response.data.data);
-          });
-        navigate("/customer/products");
-        axios.get(`/customers/${loggedInUser._id}/cart`).then((response) => {
-          setCartProducts((prev) => {
-            let data = response.data.data._cartProducts;
-            const cartProducts = data.map((cartProduct) => {
-              return { stockProduct: cartProduct, quantity: 1 };
-            });
-            return cartProducts;
-          });
-        });
-      } else if (localStorage.getItem("role") == "Merchant") {
-        navigate("/merchant/stock");
-      }
-    } else if (!loggedInUser) {
-      navigate("/");
-    }
-  }, [loggedInUser]);
+  // const [token, setToken] = useState(localStorage.getItem("token") || "");
+  // const [loggedInUser, setLoggedInUser] = useState(
+  //   localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : ""
+  // );
+  // const [role, setRole] = useState(localStorage.getItem("role") || "");
+  // const [merchant, setMerchant] = useState(
+  //   localStorage.getItem("merchant")
+  //     ? JSON.parse(localStorage.getItem("merchant"))
+  //     : ""
+  // );
+  // const [cartProducts, setCartProducts] = useState([]);
+  // const [newOrder, setNewOrder] = useState("");
+  // const [customerProducts, setCustomerProducts] = useState([]);
+  // const [error, setError] = useState("");
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (loggedInUser) {
+  //     setToken(() => {
+  //       return localStorage.getItem("token");
+  //     });
+  //     setRole(() => {
+  //       return localStorage.getItem("role");
+  //     });
+  //     if (localStorage.getItem("role") == "Customer") {
+  //       setMerchant(() => {
+  //         return JSON.parse(localStorage.getItem("merchant"));
+  //       });
+  //       axios
+  //         .get(
+  //           `/merchants/${
+  //             JSON.parse(localStorage.getItem("merchant"))._id
+  //           }/stock/products`
+  //         )
+  //         .then((response) => {
+  //           setCustomerProducts(response.data.data);
+  //         });
+  //       navigate("/customer/products");
+  //       axios.get(`/customers/${loggedInUser._id}/cart`).then((response) => {
+  //         setCartProducts((prev) => {
+  //           let data = response.data.data._cartProducts;
+  //           const cartProducts = data.map((cartProduct) => {
+  //             return { stockProduct: cartProduct, quantity: 1 };
+  //           });
+  //           return cartProducts;
+  //         });
+  //       });
+  //     } else if (localStorage.getItem("role") == "Merchant") {
+  //       navigate("/merchant/stock");
+  //     }
+  //   } else if (!loggedInUser) {
+  //     navigate("/");
+  //   }
+  // }, [loggedInUser]);
   return (
     <div className="App">
-      <UserContext.Provider
-        value={{
-          loggedInUser,
-          setLoggedInUser,
-          role,
-          setRole,
-          token,
-          setToken,
-          merchant,
-          setMerchant,
-          cartProducts,
-          setCartProducts,
-          newOrder,
-          setNewOrder,
-          customerProducts,
-          setCustomerProducts,
-          error,
-          setError,
-        }}
-      >
+      <UserContextProvider>
         <NavBar />
         <Main>
           <Routes>
@@ -173,7 +154,7 @@ function App() {
             </Route>
           </Routes>
         </Main>
-      </UserContext.Provider>
+      </UserContextProvider>
     </div>
   );
 }

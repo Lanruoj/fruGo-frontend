87a3a8b1a66aab8b96336328003f7c7ca42f41e5
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-09 15:28:08 +1100

    Write `Successfully logs in user` test

--- cypress/e2e/dev.cy.js
+++ cypress/e2e/dev.cy.js
@@ -1,11 +1,11 @@
 describe("Base test", () => {
   it("Loads home page", () => {
     cy.visit("http://localhost:3000");
-    cy.get(".sc-ezOQGI")
+    cy.get("nav")
       .should("contain", "Home")
       .should("contain", "Login")
       .should("contain", "Products");
-    cy.get(".sc-bYMpWt").should("contain", "fruGo");
+    cy.get("h1").should("contain", "fruGo");
     cy.contains("WELCOME");
   });
 });
@@ -17,35 +17,30 @@ describe("Login", () => {
   });
   it("Loads login page", () => {
     cy.contains("Login");
-    cy.get(".sc-hBxehG").should("contain", "Email:");
-    cy.get(".sc-hBxehG").should("contain", "Password:");
+    cy.get("form").should("contain", "Email:");
+    cy.get("form").should("contain", "Password:");
   });
   it("Shows error for incorrect login details", () => {
     cy.get("input[name='email']").type("{enter}");
-    cy.get(".sc-hBxehG > :nth-child(4)").should(
-      "contain",
-      "Please enter an email address"
-    );
+    cy.get("#error-message").should("contain", "Please enter an email address");
     cy.get("input[name='email']").type("incorrect_email@email.com{enter}");
-    cy.get(".sc-hBxehG > :nth-child(4)").should(
-      "contain",
-      "Please enter a password"
-    );
+    cy.get("#error-message").should("contain", "Please enter a password");
     cy.get("input[name='email']")
       .clear()
       .type("incorrect_email@email.com{enter}");
     cy.get("input[name='password']").clear().type("wrongpassword{enter}");
-    cy.get(".sc-hBxehG > :nth-child(4)").should(
-      "contain",
-      "Email address does not exist"
-    );
+    cy.get("#error-message").should("contain", "Email address does not exist");
     cy.get("input[name='email']").clear().type(Cypress.env("TEST_USER_EMAIL"));
     cy.get("input[name='password']").clear().type("wrongpassword{enter}");
-    cy.get(".sc-hBxehG > :nth-child(4)").should("contain", "Invalid password");
+    cy.get("#error-message").should("contain", "Invalid password");
+  });
+  it("Successfully logs in user", () => {
     cy.get("input[name='email']").clear().type(Cypress.env("TEST_USER_EMAIL"));
     cy.get("input[name='password']")
       .clear()
       .type(Cypress.env("TEST_USER_PASSWORD"));
     cy.get('[type="submit"]').contains("Login").click();
+    cy.get("#test-user-logo").contains("Welcome, John");
+    cy.get("button").contains("Logout");
   });
 });

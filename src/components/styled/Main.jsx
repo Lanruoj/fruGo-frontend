bd236d5a7f02e03e77024b7a0cbaff1ee819a312
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-10 13:45:40 +1100

    Add `background-image`

--- src/components/styled/Main.jsx
+++ src/components/styled/Main.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
-  margin-top: 5rem;
+  margin-top: 6.5rem;
+  min-height: 100vh;
   padding: 1rem;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
+  background-image: url("https://images.squarespace-cdn.com/content/v1/54875f9ee4b02be55de1d5ea/1591707618180-TI52Y6Q5J7VCRLRW1W3G/joker-6920.jpg?format=1500w");
 `;

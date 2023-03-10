fe9eb91b5165051dbfbfd575c336d791f0e32fbd
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-10 14:42:54 +1100

    Move `<PageHeading>` to top

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -58,91 +58,93 @@ export const Register = () => {
     }
   };
   return (
-    <RegisterContainer>
+    <>
       <PageHeading>Register</PageHeading>
-      <Form onSubmit={handleSubmit} autoComplete="off" multi>
-        <InputWrapper>
-          <Input
-            type="email"
-            name="email"
-            placeholder="Email"
-            onChange={handleChange}
-            value={formData.email}
-            autoComplete="off"
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={handleChange}
-            value={formData.password}
-            autoComplete="new-password"
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Input
-            type="username"
-            name="username"
-            placeholder="Username"
-            onChange={handleChange}
-            value={formData.username}
-            autoComplete="off"
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Input
-            type="text"
-            name="firstName"
-            placeholder="First name"
-            onChange={handleChange}
-            value={formData.firstName}
-            autoComplete="off"
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Input
-            type="text"
-            name="lastName"
-            placeholder="Last name"
-            onChange={handleChange}
-            value={formData.lastName}
-            autoComplete="off"
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Input
-            type="text"
-            name="streetAddress"
-            placeholder="Street address"
-            onChange={handleChange}
-            value={formData.streetAddress}
-            autoComplete="off"
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <Dropdown
-            name="_city"
-            id="_city"
-            onChange={handleChange}
-            value={formData._city}
-          >
-            <option value="default" disabled>
-              City
-            </option>
-            {cities &&
-              cities.map((city) => {
-                return (
-                  <option key={city._id} value={city._id}>
-                    {city.name}
-                  </option>
-                );
-              })}
-          </Dropdown>
-        </InputWrapper>
-        <Button>Register</Button>
-      </Form>
-    </RegisterContainer>
+      <RegisterContainer>
+        <Form onSubmit={handleSubmit} autoComplete="off" multi>
+          <InputWrapper>
+            <Input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+              value={formData.email}
+              autoComplete="off"
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleChange}
+              value={formData.password}
+              autoComplete="new-password"
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Input
+              type="username"
+              name="username"
+              placeholder="Username"
+              onChange={handleChange}
+              value={formData.username}
+              autoComplete="off"
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Input
+              type="text"
+              name="firstName"
+              placeholder="First name"
+              onChange={handleChange}
+              value={formData.firstName}
+              autoComplete="off"
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Input
+              type="text"
+              name="lastName"
+              placeholder="Last name"
+              onChange={handleChange}
+              value={formData.lastName}
+              autoComplete="off"
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Input
+              type="text"
+              name="streetAddress"
+              placeholder="Street address"
+              onChange={handleChange}
+              value={formData.streetAddress}
+              autoComplete="off"
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Dropdown
+              name="_city"
+              id="_city"
+              onChange={handleChange}
+              value={formData._city}
+            >
+              <option value="default" disabled>
+                City
+              </option>
+              {cities &&
+                cities.map((city) => {
+                  return (
+                    <option key={city._id} value={city._id}>
+                      {city.name}
+                    </option>
+                  );
+                })}
+            </Dropdown>
+          </InputWrapper>
+          <Button>Register</Button>
+        </Form>
+      </RegisterContainer>
+    </>
   );
 };
